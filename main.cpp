ab3e32ffbf8366cc76f3550d3b7f4114e31f0900
Ej19748 <ej19748@email.vccs.edu>
2024-10-01 16:24:10 +0000

    Did some math

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() 
-{ 
-  cout << "Hello World!\n"; 
-  return 0;
+{ int random_num1, random_num2, solution;
+ cout << "Two random numbers are generated between 1 and 999." << endl << "Add them together and enter the sum" << endl;
+ random_num1 = rand() % 999 ;
+ random_num2 = rand() % 999 ;
+    solution = random_num1 + random_num2;
+ cout << " " << random_num1 << endl; 
+ cout << "+" << random_num2 << endl;
+ cout << "_____" << endl;
+ cout << "Please press ENTER on your keyboard to check your answer." << endl;
+ cin.get();
+ cout << "The answer is: " << solution << endl;
+ return 0;
 }
