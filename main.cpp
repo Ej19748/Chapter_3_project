eb6de2ec5e4d045616c5824fbd76c3edc7389c90
Ej19748 <ej19748@email.vccs.edu>
2024-10-01 15:54:05 +0000

    Earned interest

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+//Interest rate is 4.25%
+//Times compounded is 12
+//Principal is $1000
+//Interest is $43.34
+//Amount in savings is $1043.34
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 int main() 
-{ 
-  cout << "Hello World!\n"; 
-  return 0;
+{ float principal, rate, amount, total_interest;
+  int times_compounded;
+  cout << "What is the principal? ";
+  cin >> principal;
+  cout << "What is the interest rate? ";
+  cin >> rate;
+  cout << "How many times is the interest compounded? ";
+  cin >> times_compounded;
+  rate = rate/100;
+  amount = principal * pow((1 + rate/times_compounded), times_compounded);
+  total_interest = amount - principal;
+  cout << setprecision(2) << fixed;
+  cout << "Interest Rate: " << rate*100 << "%" << endl;
+  cout << "Times Compounded: " << times_compounded << endl;
+  cout << "Principal: $" << principal << endl;
+  cout << "Interest: $" << total_interest << endl;
+    return 0;
 }
