11bd4308a7ca4e7429e15f810b2900067b607516
Ej19748 <ej19748@email.vccs.edu>
2024-10-01 15:28:03 +0000

    Made cookies

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+//1.5 cups of sugar
+//1 cup of butter
+//2.75 cups of flour
+//48 cookies
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main() 
-{ 
-  cout << "Hello World!\n"; 
-  return 0;
+{  const float SUGAR = 1.5, BUTTER = 1, FLOUR = 2.75, COOKIES = 48;
+ float num_cookies, sugar_needed, butter_needed, flour_needed;
+ cout << "How many cookies do you want to make? ";
+  cin >> num_cookies;
+sugar_needed = (SUGAR * num_cookies) / COOKIES;
+ flour_needed = (FLOUR * num_cookies) / COOKIES;
+ butter_needed = (BUTTER * num_cookies) / COOKIES;
+ cout << setprecision(2) << fixed;
+ cout << "To make " << num_cookies << " cookies, you will need: \n";
+ cout << sugar_needed << " cups of sugar\n";
+ cout << butter_needed << " cups of butter\n";
+ cout << flour_needed << " cups of flour\n";
+ return 0;
 }
